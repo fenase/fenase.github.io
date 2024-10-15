5a2d67ea64a5b6bf81fdfd8d7e809ef11ed2c00d
Federico Seckel <fseckel@dc.uba.ar>
2024-10-15 14:51:34 -0300

    basic usage

--- src/app/projects/big-excel-creator/usage/usage.component.ts
+++ src/app/projects/big-excel-creator/usage/usage.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { provideTranslocoScope } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-usage',
   templateUrl: './usage.component.html',
-  styleUrl: './usage.component.css'
+  styleUrl: './usage.component.css',
+  providers: [
+    provideTranslocoScope(
+      { scope: 'projects/BigExcelCreator/usage', alias: 'projects.BigExcelCreator.usage' },
+      { scope: 'projects/BigExcelCreator', alias: 'projects.BigExcelCreator' }
+    )
+  ],
 })
 export class UsageComponent {
+  public example: string = `using BigExcelCreator;
 
+....
+
+MemoryStream stream = new MemoryStream();
+using (BigExcelWriter excel = new(stream, DocumentFormat.OpenXml.SpreadsheetDocumentType.Workbook))
+{
+    excel.CreateAndOpenSheet("Sheet Name");
+    excel.BeginRow();
+    excel.WriteTextCell("Cell content");
+    excel.WriteTextCell(123); // write as number. This allows to use formulas.
+    excel.WriteTextCell(456);
+    excel.WriteFormulaCell("SUM(B1:C1)");
+    excel.EndRow();
+    excel.BeginRow(true);
+    excel.WriteTextCell("This row is hidden");
+    excel.EndRow();
+    excel.CloseSheet();
+}`;
 }
