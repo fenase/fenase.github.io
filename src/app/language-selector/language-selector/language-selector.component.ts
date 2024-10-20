d01f410a1b452fc3bcc63408456409002c8e317a
Federico Seckel <fseckel@dc.uba.ar>
2024-10-20 02:32:25 -0300

    reworked language selector

--- src/app/language-selector/language-selector/language-selector.component.ts
+++ src/app/language-selector/language-selector/language-selector.component.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslocoService, getBrowserLang } from '@jsverse/transloco';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-language-selector',
   templateUrl: './language-selector.component.html',
-  styles: '',
+  styleUrls: ['./language-selector.component.css'],
 })
-export class LanguageSelectorComponent {
+export class LanguageSelectorComponent implements OnInit {
   private readonly storedLanguageKey: string = "lang";
 
-  constructor(private readonly translocoService: TranslocoService) {
+  public menuItems: languageMenuItem[] = [];
+  public activeLanguage?: string;
+
+  public languagesList: Array<Record<'imgUrl' | 'code' | 'label' | 'shorthand' | 'message', string>> =
+    [
+      {
+        imgUrl: 'https://flagcdn.com/gb.svg',
+        code: 'en',
+        label: 'English',
+        shorthand: 'ENG',
+        message: "Switch language to English",
+      },
+      {
+        imgUrl: 'https://flagcdn.com/es.svg',
+        code: 'es',
+        label: 'Español',
+        shorthand: 'SPA',
+        message: "Cambiar idioma a Español"
+      },
+    ];
+
+  constructor(private readonly translocoService: TranslocoService) { }
+
+  ngOnInit(): void {
     let languageCode: string | null | undefined = localStorage.getItem(this.storedLanguageKey);
 
     if (!languageCode) {
@@ -19,33 +43,23 @@ export class LanguageSelectorComponent {
     if (languageCode && this.languagesList.map(x => x.code).includes(languageCode)) {
       this.changeLanguage(languageCode);
     }
-  }
 
-  public languagesList: Array<Record<'imgUrl' | 'code' | 'name' | 'shorthand' | 'message', string>> =
-    [
+    this.menuItems = [
       {
-        imgUrl: 'https://flagcdn.com/gb.svg',
-        code: 'en',
-        name: 'English',
-        shorthand: 'ENG',
-        message: "Switch language to English",
-      },
-      {
-        imgUrl: 'https://flagcdn.com/es.svg',
-        code: 'es',
-        name: 'Spanish',
-        shorthand: 'SPA',
-        message: "Cambiar idioma a Español"
-      },
-    ];
-
-  public get availableLanguages(): Array<Record<'imgUrl' | 'code' | 'name' | 'shorthand' | 'message', string>> {
-    return this.languagesList.filter(x => x.code != this.translocoService.getActiveLang());
+        label: this.translocoService.translate("Available Languages"),
+        items: this.languagesList,
+      }
+    ]
   }
-
 
   public changeLanguage(languageCode: string): void {
     this.translocoService.setActiveLang(languageCode);
     localStorage.setItem(this.storedLanguageKey, languageCode);
+    this.activeLanguage = languageCode;
   }
+}
+
+interface languageMenuItem extends MenuItem {
+  code?: string,
+  message?: string,
 }
