40f7854e29cf6b3ac21ad7c1318db90eec355694
Federico Seckel <fseckel@dc.uba.ar>
2024-10-20 23:04:18 -0300

    page layout

--- src/app/projects/big-excel-creator/page-layout/page-layout.component.ts
+++ src/app/projects/big-excel-creator/page-layout/page-layout.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { provideTranslocoScope } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-page-layout',
   templateUrl: './page-layout.component.html',
   styles: '',
+  providers: [
+    provideTranslocoScope(
+      { scope: 'projects/BigExcelCreator/page-layout', alias: 'projects.BigExcelCreator.pageLayout' },
+      { scope: 'projects/BigExcelCreator', alias: 'projects.BigExcelCreator' }
+    )
+  ],
 })
 export class PageLayoutComponent {
 
