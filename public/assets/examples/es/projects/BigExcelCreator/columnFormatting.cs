d4400f0de56430868e2514fba6da9698f0e0ba2a
Federico Seckel <fseckel@dc.uba.ar>
2024-10-21 03:14:07 -0300

    some progress with styling

---
+++ public/assets/examples/es/projects/BigExcelCreator/columnFormatting.cs
@@ -0,0 +1,7 @@
+List<Column> cols = new List<Column> {
+    new Column{CustomWidth = true, Width=10},   // A
+    new Column{CustomWidth = true, Width=15},   // B
+    new Column{CustomWidth = true, Width=18},   // c
+};
+
+excel.CreateAndOpenSheet("Nombre Hoja", cols);
