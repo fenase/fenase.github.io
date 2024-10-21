cd93bc0486e157dffad60e3a4ea09f8b33128ae2
Federico Seckel <fseckel@dc.uba.ar>
2024-10-20 22:40:09 -0300

    data validation

--- src/app/projects/big-excel-creator/data-validation/data-validation.component.ts
+++ src/app/projects/big-excel-creator/data-validation/data-validation.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { provideTranslocoScope } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-data-validation',
   templateUrl: './data-validation.component.html',
   styles: '',
+  providers: [
+    provideTranslocoScope(
+      { scope: 'projects/BigExcelCreator/data-validation', alias: 'projects.BigExcelCreator.DataValidation' },
+      { scope: 'projects/BigExcelCreator', alias: 'projects.BigExcelCreator' }
+    )
+  ],
 })
 export class DataValidationComponent {
 
