09f12bfaa95ef7c7e49093b6b24ab40e197bd8f8
Federico Seckel <fseckel@dc.uba.ar>
2024-10-21 22:58:53 -0300

    install

---
+++ src/app/projects/snowflake-idgenerator/globals.ts
@@ -0,0 +1,8 @@
+export const Versions = {
+
+  lastVersion: "1.4.2024.131",
+}
+
+export const Constants = {
+  lastVersionLink: "https://www.nuget.org/packages/SnowflakeIDGenerator/" + Versions.lastVersion,
+}
