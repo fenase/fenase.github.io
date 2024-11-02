fb9f25cbdae56cd810121e72c6dbda7667a44edd
Federico Seckel <fseckel@dc.uba.ar>
2024-11-02 13:29:36 -0300

    snowflake version

--- src/app/projects/snowflake-idgenerator/globals.ts
+++ src/app/projects/snowflake-idgenerator/globals.ts
@@ -1,8 +1,9 @@
 export const Versions = {
 
-  lastVersion: "1.4.2024.131",
+  lastVersion: "1.5.2024.306",
 }
 
 export const Constants = {
   lastVersionLink: "https://www.nuget.org/packages/SnowflakeIDGenerator/" + Versions.lastVersion,
+  lastVersionLinkDI: "https://www.nuget.org/packages/SnowflakeIDGenerator.DependencyInjection/" + Versions.lastVersion,
 }
