32be0e7e5a5f686291368229d92c56ac3ae13ab6
fenase <fenase@users.noreply.github.com>
2024-11-21 00:57:49 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/fenase/fenase.github.io/commit/08676698ba720002348e508df5a80aa352ade545

--- assets/examples/es/projects/BigExcelCreator/dataValidation.cs
+++ assets/examples/es/projects/BigExcelCreator/dataValidation.cs
@@ -2,7 +2,7 @@ excel.CreateAndOpenSheet("Sheet Name");
 
 ...
 
-// sólo permitir valores indicados en la hoja de nombre "vals", entre las celdas A1 y A6
+// sólo permitir que se ingresen los valores indicados en la hoja de nombre "vals", entre las celdas A1 y A6
 // cuando se ingresan datos a las celdas entre B2 y B10 de la hoja actual.
 string range = "B2:B10";
 string formula = "vals!$A$1:$A$6";
